836f36f3c6056e2bbfad935ac385b397c1982f7b
Dave Landry <landry.dave@gmail.com>
2017-02-23 11:53:03 -0500

    fixes passing string/constructor to Viz type prop

--- src/Viz.jsx
+++ src/Viz.jsx
@@ -1,5 +1,4 @@
 import React, {Component} from "react";
-import {dataFold as dataFormat} from "d3plus-viz";
 
 import {Geomap} from "d3plus-geomap";
 import {Donut, Pie, Tree, Treemap} from "d3plus-hierarchy";
@@ -40,11 +39,14 @@ class Viz extends Component {
 
     const {config, dataFormat} = this.props;
 
-    const viz = this.state.viz
-      .config(this.context.d3plus || {})
-      .config(Object.assign({}, config, {data: []}));
+    const viz = this.state.viz.config(this.context.d3plus || {});
 
-    if (config.data) viz.data(config.data, dataFormat);
+    if (dataFormat && config.data) {
+      viz
+        .config(Object.assign({}, config, {data: []}))
+        .data(config.data, dataFormat);
+    }
+    else viz.config(config);
 
     viz.render();
 
@@ -70,5 +72,5 @@ Viz.contextTypes = {
     @param {Object} [config = {}] An object containing method/value pairs to be passed to the visualization's .config( ) method.
     @param {Function} [dataFormat = d3plus.dataFold] A custom formatting function to be used when formatting data from an AJAX request. The function will be passed the raw data returned from the request, and is expected to return an array of values used for the data method.
 */
-Viz.defaultProps = {config: {}, dataFormat, viz: Treemap};
+Viz.defaultProps = {type: Treemap};
 export default Viz;
