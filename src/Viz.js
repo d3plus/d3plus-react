d807f7a0a05e10f7ca0988db303a1d89b15ab65d
cnavarreteliz <cnavarreteliz@gmail.com>
2019-07-08 10:34:07 -0400

    includes linksFormat and nodesFormat props

--- src/Viz.js
+++ src/Viz.js
@@ -42,16 +42,19 @@ class Viz extends Component {
 
   /**
       @memberof Viz
-      @desc Sets visualization config, accounting for dataFormat, and renders the visualization.
+      @desc Sets visualization config, accounting for dataFormat, linksFormat or nodesFormat, and renders the visualization.
       @private
   */
   renderViz() {
     const {viz} = this;
-    const {config, dataFormat} = this.props;
+    const {config, dataFormat, linksFormat, nodesFormat} = this.props;
     const globalConfig = this.context.d3plus || {};
     const c = assign({}, globalConfig, config);
 
     if (dataFormat && c.data) viz.config(c).data(c.data, dataFormat);
+    if (linksFormat && c.links) viz.config(c).links(c.data, linksFormat);
+    if (nodesFormat && c.nodes) viz.config(c).nodes(c.data, nodesFormat);
+
     else viz.config(c);
     viz.render();
 
@@ -79,5 +82,7 @@ Viz.defaultProps = {
     @memberof Viz
     @param {Object} [config = {}] An object containing method/value pairs to be passed to the visualization's .config( ) method.
     @param {Function} [dataFormat = d3plus.dataFold] A custom formatting function to be used when formatting data from an AJAX request. The function will be passed the raw data returned from the request, and is expected to return an array of values used for the data method.
+    @param {Function} [linksFormat = d3plus.links(path, formatter)] A custom formatting function to be used when formatting links from an AJAX request. The function will be passed the raw data returned from the request, and is expected to return an array of values used for the links method.
+    @param {Function} [nodesFormat = d3plus.nodes(path, formatter)] A custom formatting function to be used when formatting nodes from an AJAX request. The function will be passed the raw data returned from the request, and is expected to return an array of values used for the nodes method.
 */
 export default Viz;
