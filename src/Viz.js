99b7193a8df28ef87eabf40fa7044c14f32af9b5
cnavarreteliz <cnavarreteliz@gmail.com>
2019-07-08 11:01:11 -0400

    includes topojsonFormat

--- src/Viz.js
+++ src/Viz.js
@@ -47,13 +47,14 @@ class Viz extends Component {
   */
   renderViz() {
     const {viz} = this;
-    const {config, dataFormat, linksFormat, nodesFormat} = this.props;
+    const {config, dataFormat, linksFormat, nodesFormat, topojsonFormat} = this.props;
     const globalConfig = this.context.d3plus || {};
     const c = assign({}, globalConfig, config);
 
     if (dataFormat && c.data) viz.config(c).data(c.data, dataFormat);
     if (linksFormat && c.links) viz.config(c).links(c.data, linksFormat);
     if (nodesFormat && c.nodes) viz.config(c).nodes(c.data, nodesFormat);
+    if (topojsonFormat && c.nodes) viz.config(c).topojson(c.data, topojsonFormat);
 
     else viz.config(c);
     viz.render();
@@ -84,5 +85,6 @@ Viz.defaultProps = {
     @param {Function} [dataFormat = d3plus.dataFold] A custom formatting function to be used when formatting data from an AJAX request. The function will be passed the raw data returned from the request, and is expected to return an array of values used for the data method.
     @param {Function} [linksFormat = d3plus.links(path, formatter)] A custom formatting function to be used when formatting links from an AJAX request. The function will be passed the raw data returned from the request, and is expected to return an array of values used for the links method.
     @param {Function} [nodesFormat = d3plus.nodes(path, formatter)] A custom formatting function to be used when formatting nodes from an AJAX request. The function will be passed the raw data returned from the request, and is expected to return an array of values used for the nodes method.
+    @param {Function} [topojsonFormat = d3plus.topojson(path, formatter)] A custom formatting function to be used when formatting topojson from an AJAX request. The function will be passed the raw data returned from the request, and is expected to return an array of values used for the topojson method.
 */
 export default Viz;
